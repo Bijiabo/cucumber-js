fb2515bffa2b16e2580e687f1b553f54299db303
bevvvis <bevis@acflow.com>
2023-03-16 17:26:15 +0800

    feat: support test file outside current project

---
+++ src/api/replaceRequirePath.ts
@@ -0,0 +1,63 @@
+import * as babel from '@babel/core'
+import { v4 as uuid } from 'uuid'
+
+export type SourceCodeRequireReplaceLogic = (params: {
+  originalRequirePath: string
+}) => {
+  needReplace: boolean
+  newRequirePath?: string
+}
+
+export function replaceRequirePathForSourceCode(config: {
+  sourceCodeString: string
+  filename?: string
+  replaceLogic: SourceCodeRequireReplaceLogic
+}): string | undefined {
+  const { sourceCodeString, filename = uuid(), replaceLogic } = config
+  const transformedCode = babel.transform(sourceCodeString, {
+    filename: filename,
+    // presets: ['@babel/preset-env'], // Convert to ES5 code
+    plugins: [
+      [
+        '@babel/plugin-transform-modules-commonjs',
+        {
+          allowTopLevelThis: true,
+          strictMode: false,
+        },
+      ],
+      '@babel/plugin-syntax-dynamic-import',
+      {
+        visitor: {
+          CallExpression(path: any) {
+            if (
+              path.node.callee.type === 'Identifier' &&
+              path.node.callee.name === 'require' &&
+              path.node.arguments[0].type === 'StringLiteral'
+            ) {
+              const { needReplace, newRequirePath } = replaceLogic({
+                originalRequirePath: path.node.arguments[0].value,
+              })
+              if (needReplace && newRequirePath) {
+                path.node.arguments[0].value = newRequirePath
+              }
+            }
+            // remove use strict
+            const value = path.get('value')
+            if (value.isStringLiteral({ value: 'use strict' })) {
+              path.remove()
+            }
+          },
+        },
+      },
+    ],
+    parserOpts: {
+      strictMode: false,
+    },
+  })
+
+  if (transformedCode?.code) {
+    return transformedCode.code
+  }
+
+  return undefined
+}
--- src/api/support.ts
+++ src/api/support.ts
@@ -1,10 +1,61 @@
+import fs from 'fs'
+import path from 'path'
+import { createHash } from 'crypto'
+import { v4 as uuid } from 'uuid'
 import { IdGenerator } from '@cucumber/messages'
 import { ISupportCodeLibrary } from '../support_code_library_builder/types'
 import supportCodeLibraryBuilder from '../support_code_library_builder'
 import { pathToFileURL } from 'url'
+import { replaceRequirePathForSourceCode } from './replaceRequirePath'
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { importer } = require('../importer')
+
+function isAbsolutePath(path: string): boolean {
+  return path.startsWith('/') || /^[a-zA-Z]:\\/.test(path)
+}
+
+function isRelativePath(path: string): boolean {
+  return path.startsWith('./') || path.startsWith('../')
+}
+
+function isPath(path: string): boolean {
+  return isAbsolutePath(path) || isRelativePath(path)
+}
+
+function isSubdirectory(params: {
+  subdirectory: string
+  directory: string
+}): boolean {
+  const { subdirectory, directory } = params
+  const relativePath = path.relative(subdirectory, directory)
+  return !relativePath.startsWith('..') && !path.isAbsolute(relativePath)
+}
+
+/**
+ * Find the nearest node_modules directory in the parent directories of the specified file path.
+ * @param filePath The file path to start searching from.
+ * @returns The absolute path to the nearest node_modules directory or null if not found.
+ */
+export function findNearestNodeModules(filePath: string): string | null {
+  let currentDir = path.dirname(path.resolve(filePath))
+
+  while (true) {
+    const nodeModulesPath = path.join(currentDir, 'node_modules')
+
+    if (fs.existsSync(nodeModulesPath)) {
+      return nodeModulesPath
+    }
+
+    const parentDir = path.dirname(currentDir)
+
+    if (parentDir === currentDir) {
+      return null
+    }
+
+    currentDir = parentDir
+  }
+}
 
 export async function getSupportCodeLibrary({
   cwd,
@@ -25,7 +76,63 @@ export async function getSupportCodeLibrary({
     importPaths,
   })
   requireModules.map((module) => require(module))
-  requirePaths.map((path) => require(path))
+
+  // todo: 增加一段英文注释
+  const pathForNodeModulesDir = findNearestNodeModules(__dirname)
+  const pathForProjectRoot = path.join(pathForNodeModulesDir, '..')
+  for (let requirePath of requirePaths) {
+    const isInCurrentProjectDir = isSubdirectory({
+      directory: pathForProjectRoot,
+      subdirectory: requirePath,
+    })
+
+    if (isInCurrentProjectDir) {
+      require(requirePath)
+    } else {
+      // load file source code
+      let sourceCodeString = fs.readFileSync(requirePath, 'utf8')
+      // generate fake filename by file path
+      let fileName = uuid()
+      {
+        const md5 = createHash('md5')
+        md5.update(requirePath)
+        fileName = md5.digest('hex')
+      }
+      // replace require logic
+      sourceCodeString = replaceRequirePathForSourceCode({
+        sourceCodeString,
+        replaceLogic: ({ originalRequirePath }) => {
+          if (isPath(originalRequirePath)) {
+            return {
+              needReplace: false,
+            }
+          } else {
+            const npmPackagePathInCurrentNodeModules = path.join(
+              pathForNodeModulesDir,
+              originalRequirePath
+            )
+            const packageInstalledInCurrentProject = fs.existsSync(
+              npmPackagePathInCurrentNodeModules
+            )
+            if (packageInstalledInCurrentProject) {
+              return {
+                needReplace: true,
+                newRequirePath: npmPackagePathInCurrentNodeModules,
+              }
+            } else {
+              return {
+                needReplace: false,
+              }
+            }
+          }
+        },
+      })
+
+      // @ts-ignore
+      const mod = new module.constructor()
+      mod._compile(sourceCodeString, fileName)
+    }
+  }
   for (const path of importPaths) {
     await importer(pathToFileURL(path))
   }
